4cf6b4e642eaa51be0565a9d8d4143c2050291a6
Corbin <140107765+Promingy@users.noreply.github.com>
2024-05-18 15:17:38 -0600

    more lights set up - next up is fire animations

--- main.js
+++ main.js
@@ -66,18 +66,18 @@ controls.minPolarAngle = Math.PI / 5;
 controls.maxAzimuthAngle = Math.PI / 10;
 controls.minAzimuthAngle = -Math.PI / 1.65;
 
-const pointLight = new Three.PointLight("orange");
+const pointLight = new Three.PointLight(0xF07F13);
 const pointLight2 = pointLight.clone();
 const pointLight3 = pointLight.clone();
 
 pointLight.position.set(44, 50, 80);
-pointLight.intensity = 1000;
+pointLight.intensity = 500;
 
 pointLight2.position.set(44, 50, -30);
-pointLight2.intensity = 1000;
+pointLight2.intensity = 500;
 
 pointLight3.position.set(-25, 50, -60);
-pointLight3.intensity = 1000;
+pointLight3.intensity = 500;
 
 const pointLight4 = pointLight.clone();
 pointLight4.position.set(80, 50, 70);
@@ -85,24 +85,22 @@ pointLight4.position.set(80, 50, 70);
 const pointLight5 = pointLight4.clone();
 pointLight5.position.set(80, 50, -20)
 
-const  rectLight = new Three.RectAreaLight( "orange", 1000, 20, 15);
+const  rectLight = new Three.RectAreaLight( "orange", 100, 20, 15);
 rectLight.position.set(-33.5, 10, -73);
-rectLight.rotateX(3.15);
-rectLight.power = 100000;
+rectLight.rotateX(3.14);
 
-const pointLightHelper = new Three.PointLightHelper(pointLight5, 1);
+scene.add(pointLight, pointLight2, pointLight3, rectLight);
 
-scene.add(pointLight, pointLight2, pointLight3, rectLight, pointLight4, pointLight5, pointLightHelper);
+// new directional light
+const directionalLight = new Three.DirectionalLight("orange", .01);
+directionalLight.position.set(0, 0, -200);
+// directionalLight.position.set(-1000, 200, 1000);
 
-// // new directional light
-// const directionalLight = new Three.DirectionalLight(0xffffff, 0.25);
-// directionalLight.position.set(0, 0, -200);
+// directionalLightHelper
+const directionalLightHelper = new Three.DirectionalLightHelper(directionalLight, 5);
+scene.add(directionalLightHelper);
 
-// //directionalLightHelper
-// const directionalLightHelper = new Three.DirectionalLightHelper(directionalLight, 5);
-// scene.add(directionalLightHelper);
-
-// scene.add(directionalLight);
+scene.add(directionalLight);
 
 scene.background = new Three.Color(0xE1C699);
 
