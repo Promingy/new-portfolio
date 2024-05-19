da9ad840bf4672ed2c439f35f021f1236232f179
Corbin <140107765+Promingy@users.noreply.github.com>
2024-05-19 16:03:11 -0600

    added floating island model

--- main.js
+++ main.js
@@ -29,6 +29,13 @@ loader.load('models/newTav.glb', function(tavern) {
   scene.add(tavern.scene);
 })
 
+loader.load('models/floating_island.glb', function(island) {
+
+  island.scene.position.set(-15, 2, 10)
+  island.scene.scale.set(20, 20, 20);
+  scene.add(island.scene);
+})
+
 // loader.load('models/medieval_book.glb', function(book) {
 //   book.scene.scale.set(0.1, 0.1, 0.1);
 //   book.scene.position.setY(10)
@@ -93,7 +100,7 @@ camera.position.setX(-100);
 renderer.render(scene, camera);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.maxDistance = 300;
+controls.maxDistance = 700;
 controls.minDistance = 150;
 controls.enablePan = false;
 controls.maxPolarAngle = Math.PI / 2.2;
@@ -106,25 +113,20 @@ const pointLight2 = pointLight.clone();
 const pointLight3 = pointLight.clone();
 
 pointLight.position.set(44, 50, 80);
-pointLight.intensity = 500;
+pointLight.intensity = 1000;
 
 pointLight2.position.set(44, 50, -30);
-pointLight2.intensity = 500;
+pointLight2.intensity = 1000;
 
 pointLight3.position.set(-25, 50, -60);
-pointLight3.intensity = 500;
+pointLight3.intensity = 1000;
 
-const pointLight4 = pointLight.clone();
-pointLight4.position.set(80, 50, 70);
-
-const pointLight5 = pointLight4.clone();
-pointLight5.position.set(80, 50, -20)
 
 const  rectLight = new Three.RectAreaLight( "orange", 100, 20, 15);
 rectLight.position.set(-33.5, 10, -73);
 rectLight.rotateX(3.14);
 
-var hemiLight = new Three.HemisphereLight( 0xffffff, 0x444444, .5);
+var hemiLight = new Three.HemisphereLight( 0xffffff, 0x444444, .25);
 hemiLight.position.set( 0, 300, 0 );
 
 
@@ -134,7 +136,6 @@ const directionLight2 = directionalLight.clone();
 directionalLight.position.set(-1000, 500, 1000);
 // directionLight2.position.set(1000, 250, -1000);
 
-const directionalLightHelper = new Three.DirectionalLightHelper(directionLight2, 5);
 
 scene.add(directionalLight, directionLight2);
 scene.add(pointLight, pointLight2, pointLight3, rectLight, hemiLight);
